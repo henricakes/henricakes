16df50ce992b3b39cb824d242d33b42ebb13828b
henricakes <angasjohnhenry@gmail.com>
2025-06-24 10:10:06 +0800

    Added new functions for HeroCarousel

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -1,27 +1,38 @@
 // src/components/HeroCarousel.jsx
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
 import hero1 from "../assets/HERO_1.png";
-// Add more images if needed
-
+import hero2 from "../assets/HERO_2.png";
 const HeroCarousel = () => {
   return (
     <Swiper
-      modules={[Autoplay, Pagination]}
+      modules={[Autoplay]}
       autoplay={{ delay: 4000 }}
       loop={true}
-      pagination={{ clickable: true }}
-      className="h-full w-full"
+      className="mt-10 h-full w-full"
     >
       <SwiperSlide>
-        <img
-          src={hero1}
-          alt="Slide 1"
-          className="w-full h-full object-contain max-h-screen"
-        />
+        <div className="w-full h-full flex items-center justify-center">
+          <img
+            loading="lazy"
+            src={hero1}
+            alt="Slide 1"
+            className="w-full h-screen object-contain max-h-screen"
+          />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="w-full h-full flex items-center justify-center">
+          <img
+            loading="lazy"
+            src={hero2}
+            alt="Slide 2"
+            className="w-full h-screen object-contain max-h-screen"
+          />
+        </div>
       </SwiperSlide>
       {/* Add more <SwiperSlide> blocks as needed */}
     </Swiper>
