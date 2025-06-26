67aafac430f1319f18a4f3c02562cd635ea19c3f
henricakes <angasjohnhenry@gmail.com>
2025-06-26 11:49:55 +0800

    Added some new assets | Updated TechSkills

--- src/components/TechSkills.jsx
+++ src/components/TechSkills.jsx
@@ -14,6 +14,9 @@ import FIGMA from "../assets/FIGMA.svg";
 
 import PHOTOSHOP from "../assets/PHOTOSHOP.svg";
 import CS6 from "../assets/PHOTOSHOP_CS6.svg";
+import LIGHTROOM from "../assets/LIGHTROOM.svg";
+import SNAPSEED from "../assets/SNAPSEED.svg";
+import FILMORA from "../assets/FILMORA.svg";
 
 const TechSkills = () => {
   return (
@@ -53,9 +56,9 @@ const TechSkills = () => {
       {/* --- CREATIVE STACK SECTION --- */}
       <section
         id="creative-skills"
-        className="relative bg-licorice text-white min-h-screen scroll-mt-20 pt-20"
+        className="relative bg-licorice text-white min-h-screen scroll-mt-20 pt-20 pb-10"
       >
-        <div className="absolute top-0 inset-x-0 h-32 bg-gradient-to-b from-charcoal to-transparent z-10" />
+        <div className="absolute top-0 inset-x-0 h-32 bg-gradient-to-b from-charcoal to-transparent z-10 " />
 
         <div className="relative z-20 flex flex-col justify-center items-center space-y-6 px-4">
           <h3 className="text-3xl font-bold text-center">CREATIVE STACK</h3>
@@ -67,12 +70,15 @@ const TechSkills = () => {
             <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
               {/* Replace with creative tools later */}
               <TechContainer label={"Photoshop"} icon={PHOTOSHOP} />
-              <TechContainer label={"Photoshop CS6"} icon={CS6} />
+              <TechContainer label={"CS6"} icon={CS6} />
+              <TechContainer label={"Lightroom"} icon={LIGHTROOM} />
+              <TechContainer label={"Snapseed"} icon={SNAPSEED} />
+              <TechContainer label={"Filmora X"} icon={FILMORA} />
             </div>
           </section>
         </div>
 
-        <div className="absolute bottom-0 inset-x-0 h-48 bg-gradient-to-t from-charcoal via-charcoal to-transparent z-10" />
+        <div className="absolute bottom-0 inset-x-0 h-48 bg-gradient-to-t from-charcoal via-charcoal to-transparent z-10 " />
       </section>
     </>
   );
