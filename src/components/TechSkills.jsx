d051f1383e867a8f927b22052fec09c45e9adfc3
henricakes <angasjohnhenry@gmail.com>
2025-06-27 14:27:14 +0800

    tamad.

--- src/components/TechSkills.jsx
+++ src/components/TechSkills.jsx
@@ -80,6 +80,7 @@ const TechSkills = () => {
 
         <div className="absolute bottom-0 inset-x-0 h-48 bg-gradient-to-t from-charcoal via-charcoal to-transparent z-10 " />
       </section>
+      {/* Latest WOrk: TInatamad for today (June 27, 2025) Bawi ako bukas. */}
     </>
   );
 };
