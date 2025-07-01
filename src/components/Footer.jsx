4cc4a3b130123852bb7daed1a505029bab8cd53f
henricakes <angasjohnhenry@gmail.com>
2025-07-01 18:16:20 +0800

    Added some new breakpoints on tailwind config | added new assets | Footer - to be continued

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,44 @@
 import { herocakes } from "../constants";
+import GITHUB_SOLID from "../assets/GITHUB_SOLID.svg";
+import LINKEDIN from "../assets/LINKEDIN.svg";
 
 const Footer = () => {
   return (
-    <footer className="flex-row bg-smoke-500 text-white py-4 text-center rounded-l-lg rounded-r-lg">
-      <div className="flex ml-5 items-center flex-shrink-0 gap-2">
-        <img className="h-10 w-10 mr-2" src="/henricakes.svg" alt="Logo" />
-        <span className="text-2xl font-bold tracking-tight">
-          <a href={herocakes}>Henricakes</a>
-        </span>
+    <footer className="bg-smoke-500 text-white py-4 px-6 rounded-full border-t border-t-slate-500">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-3">
+        {/* LEFT: Logo and Name */}
+        <div className="flex items-center gap-2">
+          <img className="h-10 w-10 mr-2" src="/henricakes.svg" alt="Logo" />
+          <span className="text-2xl font-bold tracking-tight">
+            <a href={herocakes}>Henricakes</a>
+          </span>
+          <div></div>
+        </div>
+        <div>
+          <p className="font-bold md:hidden">Connect with me</p>
+        </div>
+        <div className="flex items-center gap-4">
+          <a
+            href="https://www.linkedin.com/in/henricakes/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={LINKEDIN} alt="GitHub" className="w-10 h-10" />
+          </a>
+          <a
+            href="https://github.com/henricakes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={GITHUB_SOLID} alt="GitHub" className="w-7 h-7" />
+          </a>
+        </div>
       </div>
 
-      <p className="flex justify-center text-sm">
+      {/* BOTTOM: Copyright */}
+      <p className="mt-2 text-sm text-center">
         &copy; {new Date().getFullYear()}&nbsp;
-        <a href={herocakes}>Henricakes.</a> {/* Sonar Warning */}
-        &nbsp; All rights reserved.
+        <a href={herocakes}>Henricakes.</a>&nbsp; All rights reserved.
       </p>
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        m_xs: "350px",
+        m_md: "375px",
+        m_lg: "425px",
+        "3xl": "1600px",
+        "4k": "2560px",
+      },
       colors: {
         licorice: "#16110",
         obsidian: "#08100C",
