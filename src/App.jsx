a1366b4134695fe64179aa9c7ce2d78aeaf28fd7
henricakes <86226505+henricakes@users.noreply.github.com>
2025-07-25 11:01:05 +0800

    Some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import TechSkills from "./components/TechSkills";
 import Projects from "./components/Projects";
-
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 const App = () => {
+  useEffect(() => {
+    // Prevent browser from restoring scroll position
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+
+    // Scroll to top before reload
+    window.onbeforeunload = () => {
+      window.scrollTo(0, 0);
+    };
+  }, []);
+
   return (
     <>
       <NavBar />
